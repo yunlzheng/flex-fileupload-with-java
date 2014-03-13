bf4646595071621b44ed8a4a8221752fb8c660e0
yunlzheng <yunl.zheng@gmail.com>
2014-03-13 11:38:37 +0800

    define error code eunm

---
+++ src/main/java/com/github/fileupload/servlet/UploaderServlet.java
@@ -0,0 +1,51 @@
+package com.github.fileupload.servlet;
+
+import com.github.fileupload.servlet.action.ActionException;
+import com.github.fileupload.servlet.action.FileCheckAction;
+import com.github.fileupload.servlet.action.OrdninaryUploadAction;
+import com.github.fileupload.servlet.action.UploadAction;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class UploaderServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.process(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.process(req,resp);
+    }
+
+    protected void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter out = resp.getWriter();
+        UploadAction paser = this.loadUploadRequestPaser(req);
+        try {
+            paser.paser(req, out);
+        } catch (ActionException e) {
+            out.print("<error id=\"10\">ActionException</error>");
+        }
+
+    }
+
+    private UploadAction loadUploadRequestPaser(HttpServletRequest req) {
+        String fileAction = req.getParameter("fileAction");
+        if (fileAction.equals("check")) {
+            return new FileCheckAction();
+        } else if(fileAction.equals("upload")) {
+            return new OrdninaryUploadAction();
+        }
+        else {
+            return new OrdninaryUploadAction();
+        }
+    }
+
+}
---
+++ src/main/java/com/github/fileupload/servlet/action/ErrorCode.java
@@ -0,0 +1,55 @@
+package com.github.fileupload.servlet.action;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum ErrorCode {
+
+    FILE_ALREADY_EXIST(410, "File already exists"),
+    FILE_SIZE_EXCEED_MAXIMUM(420, "File size exceeds maximum"),
+    FILE_TYPE_NOT_ALLOWED(430, "File type not allowed"),
+    CLOULD_NOT_OPEN_FILE(510, "Could not open file for writing"),
+    FILE_SEEK_FAILED(515, "File seek operation failed"),
+    COULD_NOT_WRITE_FILE(520, "Could not write to file"),
+    TRUNCATE_FILE_FAILED(525, "Truncate file failed"),
+    COUNLD_NOT_WIRTE_TMP_FILE_HEADER(530, "Could not write temporary file header"),
+    FILE_CLOSE_ERROR(540, "File could not be closed"),
+    TMPFILE_CLOULD_NOT_RENAME(550, "Temporary file could not be renamed to original"),
+    FILE_DELETED_FAILED(555, "File could not be deleted"),
+    COMPRESSION_NOT_SUPPORT(560, "Data compression not supported (ZLib)");
+
+    private int errorCode;
+    private String message;
+
+    private static Map<Integer, String> codeMap = new HashMap<Integer, String>();
+
+    static{
+
+        for(ErrorCode error:ErrorCode.values()){
+            codeMap.put(error.getErrorCode(), error.getMessage());
+        }
+
+    }
+
+     public static String geteErrorMessage(int code) {
+        if(codeMap.containsKey(code)){
+            return codeMap.get(code);
+        }else{
+            return "unkown error!";
+        }
+     }
+
+    private ErrorCode(int errorCode, String message) {
+        this.errorCode = errorCode;
+        this.message = message;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}
